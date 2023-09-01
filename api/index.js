dc0088fbb92c0c8bc7ff17bdaf9cd6c89396eb3c
Jake <jake.browne9@gmail.com>
2023-09-01 10:39:27 +0800

    first commit

---
+++ api/index.js
@@ -0,0 +1,133 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const User = require('./models/User');
+const ws = require('ws');
+
+dotenv.config();
+mongoose.connect(process.env.MongoDb);
+const jwtSecret = process.env.jwtSecret;
+const bcSalt = bcrypt.genSaltSync(10);
+
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    credentials: true,
+    origin: process.env.ClientURL
+}));
+
+app.get('/profile', (req,res) => {
+    const token = req.cookies?.token;
+    if(token) {
+        jwt.verify(token, jwtSecret, {}, (err, userData) => {
+            if(err) {
+                console.error(err)
+            }
+            res.json(userData)
+        })
+
+    } else {
+        res.status(401).json('no token');
+    }
+});
+
+app.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    try {
+        const foundUser = await User.findOne({username});
+        if(foundUser) {
+            const passOk = bcrypt.compareSync(password, foundUser.password);
+            if(passOk) {
+                jwt.sign({userId:foundUser._id, username}, jwtSecret, {} , (err, userData) => {
+                    res.cookie('token', token, {sameSite:'none', secure:true}).json({
+                        id:foundUser._id
+                    })
+                })
+            }
+        }
+
+        jwt.sign({userId: foundUser._id, username}, jwtSecret, {}, (err, token) => {
+            if (err) {
+                console.log('Error: ',err)
+            }
+            res.cookie('token', token, {sameSite:'none', secure:true}).status(201).json({
+                id: foundUser._id,
+                username
+                
+            })
+        }) 
+
+    } catch(err) {
+        console.error(err)
+    }
+    });
+
+app.post('/register', async (req, res) => {
+    const {username, password} = req.body;
+    try {
+        const hashedPW = bcrypt.hashSync(password, bcSalt);
+        const user = await User.create({
+            username: username, 
+            password: hashedPW});
+        jwt.sign({userId: user._id, username}, jwtSecret, {}, (err, token) => {
+            if (err) {
+                console.log('Error: ',err)
+            }
+            res.cookie('token', token, {sameSite:'none', secure:true}).status(201).json({
+                id: user._id,
+                username
+                
+            })
+        }) 
+
+    } catch(err) {
+        console.error(err)
+    }
+     
+        
+       
+        
+    
+
+})
+const server = app.listen(4040);
+
+const wss = new ws.WebSocketServer({server});
+wss.on('connection', (connection, req) => {
+    const cookies = req.headers.cookie;
+    if (cookies) {
+        const tokenCookieString = cookies.split(';').find(str => str.startsWith('token='))
+        if (tokenCookieString) {
+            const token = tokenCookieString.split('=')[1];
+            if(token) {
+                jwt.verify(token, jwtSecret, {}, (err, userData)=> {
+                    if(err) console.log(err)                    
+
+                    const {userId, username} = userData;
+                    connection.userId = userId;
+                    connection.username = username;
+
+
+                })
+            }
+        }
+    }
+
+    // notify everyone about online users when someone connects
+    [...wss.clients].forEach(client => {
+        client.send(JSON.stringify( {
+            onlineUserData: [...wss.clients].map( c => ( {
+                userId: c.userId, username: c.username
+                
+            }))
+    }))
+    })
+})
+
+// https://www.youtube.com/watch?v=mYy-d6BtqmU&t=39s&ab_channel=CodingWithDawid
---
+++ client/src/Chat.jsx
@@ -0,0 +1,126 @@
+import Avatar from "./Avatar";
+import Logo from "./Logo";
+import { UserContext } from "./UserContext";
+import { useContext, useEffect, useState } from "react";
+export default function Chat() {
+  const { setUsername, setId } = useContext(UserContext);
+  const [ws, setWs] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const [selectedUserId, setSelectedUserId] = useState(null);
+  const [newMessage, setNewMessage] = useState("");
+  const { username, id } = useContext(UserContext);
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:4040");
+    setWs(ws);
+    ws.addEventListener("message", handleMessage);
+  }, []);
+
+  function showOnlineUsers(usersArr) {
+    const users = {};
+    usersArr.forEach(({ userId, username }) => {
+      users[userId] = username;
+    });
+    setOnlineUsers(users);
+  }
+
+  function handleMessage(e) {
+    
+    const messageData = JSON.parse(e.data);
+
+    if ("onlineUserData" in messageData) {
+      showOnlineUsers(messageData.onlineUserData);
+    }
+  }
+
+  function sendMessage(e) {
+    e.preventDefault();
+    ws.send(
+      JSON.stringify({
+        message: {
+          recipient: selectedUserId,
+          text: newMessage,
+        },
+      })
+    );
+  }
+
+  const onlineUsersExclOurUser = { ...onlineUsers };
+  delete onlineUsersExclOurUser[id];
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          setUsername(null);
+          setId(null);
+        }}
+      >
+        Clear user context state
+      </button>
+
+      <div className="flex h-screen">
+        <div className="bg-white w-1/3">
+          <Logo />
+          {Object.keys(onlineUsersExclOurUser).map((userId) => (
+            <div
+              key={userId}
+              onClick={() => setSelectedUserId(userId)}
+              className={
+                "border-b border-gray-100 flex items-center gap-2 cursor-pointer " +
+                (userId === selectedUserId ? "bg-blue-50" : "")
+              }
+            >
+              {userId === selectedUserId && (
+                <div className="w-1 bg-blue-500 h-12 rounded-r-md"></div>
+              )}
+              <div className="flex gap-2 py-2 pl-4 items-center">
+                <Avatar username={onlineUsers[userId]} userId={userId} />
+                <span className="text-gray-800">{onlineUsers[userId]}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-col bg-blue-50 w-2/3 p-2">
+          <div className="flex-grow">
+            {!selectedUserId && (
+              <div className="flex h-full items-center justify-center">
+                <div className="text-gray-300">
+                  &larr; Select a friend to chat!
+                </div>
+              </div>
+            )}
+          </div>
+          {!!selectedUserId && (
+            <form className="flex gap-2" onSubmit={sendMessage}>
+            <input
+              type="text"
+              value={newMessage}
+              onChange={(e) => setNewMessage(e.target.value)}
+              placeholder="Type your message here"
+              className="bg-white flex-grow border rounded-sm p-2"
+            />
+            <button type="submit" className="bg-blue-500 p-2 text-white">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+                />
+              </svg>
+            </button>
+          </form>
+          )}
+          
+        </div>
+      </div>
+    </>
+  );
+}
